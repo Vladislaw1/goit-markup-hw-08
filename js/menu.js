402555f572dd77a62030a2bd150b769822a35e32
Vladislaw1 <vladzajcenko077@gmail.com>
2021-02-13 13:47:11 +0200

    Адаптация сайта

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,6 @@
     refs.openMenuBtn.addEventListener("click", toggleModal);
     refs.closeMenuBtn.addEventListener("click", toggleModal);
     function toggleModal() {
-      refs.menu.classList.toggle("is-menu-hidden");
+      refs.menu.classList.toggle("is-open");
     }
   })();
